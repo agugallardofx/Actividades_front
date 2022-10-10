e5ab869fa0a71536ff1f9ec719ae451be20170c4
agugallardofx <agusgallardo25@gmail.com>
2022-10-10 19:02:55 -0300

    Clase 10

---
+++ Clase 10/js/main.js
@@ -0,0 +1,63 @@
+console.log('hola mundo');
+//Ejemplos
+// // VARIABLES
+// var apellido = "Gallardo"
+
+// console.log(apellido);
+
+// // con comillas simples o dobles es un string
+// let nombre = "Agustin"
+// const NOMBRE = "Harry"
+
+// console.log(nombre);
+
+// // sin comillas un numero
+// let edad = 25;
+// const NUM = 10.2;
+
+//Actividad clase 10
+let nombre = "Agustin";
+
+let apellido = "Gallardo";
+
+let edad = "25";
+
+let mascota = true;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(mascota);
+
+//OPERACIONES
+
+let numeroA = 12;
+let numeroB = 3;
+
+let resultadoSuma = numeroA + numeroB;
+console.log(resultadoSuma);
+
+let resultadoMult = numeroA * numeroB;
+console.log(resultadoMult);
+
+//concatenar textos o dar espacios utilizando sumas
+
+let textoA = "Hola";
+let textoB = "Mundo";
+const BLANCO = " ";
+
+let resultadoA = textoA + BLANCO + textoB;
+//let resultadoA = textoA + " " + textoB(otra variable para espacios)
+console.log(resultadoA);
+
+// --->PROMT 
+//Ejemplo 1
+/**let entrada = prompt ("Ingrese su nombre");
+let salida = "Hola" + " " + entrada;
+alert(salida); */
+//Ejemplo 2
+let numero = parseInt(prompt("Ingrese un numero"));
+const numeroC = 30;
+let salida = numero + numeroC;
+alert(salida);
+
